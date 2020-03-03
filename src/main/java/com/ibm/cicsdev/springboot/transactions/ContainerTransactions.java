f012d9b4d5a28ecbd0e45180d67d9313eaac87b0
Chris Atkinson <atkinchr@uk.ibm.com>
2020-03-03 13:55:36 +0000

    Remove devtools from Gradle build Update ContainerTransaction class to rollback if TSQ write fials

--- src/main/java/com/ibm/cicsdev/springboot/transactions/ContainerTransactions.java
+++ src/main/java/com/ibm/cicsdev/springboot/transactions/ContainerTransactions.java
@@ -12,22 +12,22 @@ import com.ibm.cics.server.LengthErrorException;
 import com.ibm.cics.server.NotAuthorisedException;
 import com.ibm.cics.server.TSQ;
 @Component
-public class ContainerTransactions {
-	@Transactional
-	public void exampleTransactional(String... text) throws InvalidRequestException, IOErrorException, LengthErrorException, InvalidSystemIdException, ISCInvalidRequestException, ItemErrorException, NotAuthorisedException {
-
-
-	    // Set up our TSQ to be ready to write
-		TSQ targetQueue = new TSQ();
-		targetQueue.setName("EXAMPLE");
-
-		
-		for (String texts : text) {
-		   if (texts.contains("error")) {
-			   throw new RuntimeException("Texts contains error so rollback"); 
-		   }
-		   targetQueue.writeString("Writing " + texts);
-		
+public class ContainerTransactions { 
+	@Transactional	public void exampleTransactional(String... text) 
+	{     // Set up our TSQ to be ready to write
+		TSQ targetQueue = new TSQ(); targetQueue.setName("EXAMPLE"); 
+		for (String texts : text) {    
+			if (texts.contains("error")) {    
+				throw new RuntimeException("Texts contains error so rollback");    
+			}    
+			try { targetQueue.writeString("Writing " + texts); 
+			} 
+			catch (InvalidRequestException | IOErrorException | LengthErrorException | InvalidSystemIdException | ISCInvalidRequestException | ItemErrorException | NotAuthorisedException e) {
+				e.printStackTrace(); 
+				throw new RuntimeException("TSQ Write Failed"); 
+				} 
+			} 
 		}
 	}
-}
+
+
